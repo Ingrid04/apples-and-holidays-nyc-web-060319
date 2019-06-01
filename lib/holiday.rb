07d0f28b0eb462013af81ba401b5eacd48816884
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-01 05:54:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,6 +19,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
      }
    }
      holiday_supplies[:summer][:fourth_of_july][1]    # return the second element in the 4th of July array
+     
      second_supply_for_fourth_of_july(holiday_supplies)
 end
 
