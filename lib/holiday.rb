bdc00ab1c0215893449ae91b9060e843a7fdfa0a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-01 06:04:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,9 +18,11 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
-    puts holiday_supplies[:summer][:fourth_of_july][1]    # return the second element in the 4th of July array
-end 
-     
+    puts holiday_supplies[:summer][:fourth_of_july][1]
+    
+    end 
+end
+
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
     holiday_hash.each do |season, holiday|
